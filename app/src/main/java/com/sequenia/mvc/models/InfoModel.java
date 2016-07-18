524aaf9a37f3702295f158fbed36d03dc5d5a425
4ybakut2004 <4ybakut2004@gmail.com>
2016-07-18 18:26:22 +0600

    Комментарии

--- app/src/main/java/com/sequenia/mvc/models/InfoModel.java
+++ app/src/main/java/com/sequenia/mvc/models/InfoModel.java
@@ -6,15 +6,33 @@ import com.sequenia.sequeniamvc.MVC;
 import java.util.List;
 
 /**
+ * Описание модели для загрузки каких-то данных (Info)
+ *
  * Created by chybakut2004 on 14.07.16.
  */
 
 public interface InfoModel {
 
+    /**
+     * Получить данные
+     * @param tryIndex номер попытки
+     * @param infoListener успех
+     * @param errorListener ошибка
+     */
     void getInfo(int tryIndex, InfoListener infoListener, MVC.Model.ModelErrorListener errorListener);
 
+    /**
+     * Получить список данных
+     * @param tryIndex номер попытки
+     * @param infoListListener успех
+     * @param errorListener ошибка
+     */
     void getInfoList(int tryIndex, InfoListListener infoListListener, MVC.Model.ModelErrorListener errorListener);
 
+    /**
+     * Получить данные из кеша
+     * @param infoListListener успех
+     */
     void getInfoListFromCache(InfoListListener infoListListener);
 
     interface InfoListListener {
--- app/src/main/java/com/sequenia/mvc/views/InfoListView.java
+++ app/src/main/java/com/sequenia/mvc/views/InfoListView.java
@@ -5,16 +5,21 @@ import com.sequenia.mvc.objects.Info;
 import java.util.List;
 
 /**
+ * Операции экрана со список информации
+ *
  * Created by chybakut2004 on 15.07.16.
  */
 
 public interface InfoListView extends AppView {
 
+    /**
+     * Отображает список с информацией на экране
+     * @param infoList список с информацией
+     */
     void showInfoList(List<Info> infoList);
 
     void setContentVisibility(boolean visibility);
     void setLoadingVisibility(boolean visibility);
     void setEmptyScreenVisibility(boolean visibility);
-
     void setRefreshButtonEnabled(boolean enabled);
 }
